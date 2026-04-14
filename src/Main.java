2696afb874ef01e05070479ea0af3dd7f2535ae7
whoischabola <ruizcallejonisabel@gmail.com>
2026-04-14 23:42:03 +0200

    first half of querys (a-i) done

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import dao.ClienteDAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -21,7 +23,70 @@ void main() throws SQLException {
     dao.insertarCliente(c);
 
     System.out.println("Proceso finalizado");
-}
+
+    ClienteDAO dao2 = new ClienteDAO();
+    dao2.eliminarCliente(c);
+
+    Cliente c3 = new Cliente();
+    int idBuscar = 10;
+    Cliente encontrado = dao.obtenerCliente(idBuscar);
+
+    if(encontrado != null){
+        System.out.println("Cliente encontrado");
+        System.out.println("Nombre: " + encontrado.getNombre());
+    } else {
+        System.out.println("Cliente no encontrado");
+    }
+    Cliente c4 = new Cliente();
+    List<Cliente> Lista = dao.obtenerTodosLosClientes();
+
+    if (Lista.isEmpty()) {
+        System.out.println("No se encuentran clientes");
+    } else {
+        System.out.println("Lista de clientes completa");
+    }
+    ClienteDAO dao3 = new ClienteDAO();
+    List<Cliente> jovenes = dao3.obtenerMayoresDe30();
+
+    System.out.println("Mayores de 30");
+
+    if (jovenes.isEmpty()) {
+        System.out.println("No se encuentran clientes");
+    } else {
+        for (Cliente j : jovenes) {
+            System.out.println("Nombre: " + j.getNombre());
+            System.out.println("Edad: " + j.getEdad());
+        }
+            ClienteDAO dao4 = new ClienteDAO();
+            List<Cliente> vips = dao.obtenerClientesGastoMayor500();
+
+            System.out.println("Clientes que han gastado más de 500€");
+            for (Cliente cliente : vips) {
+                System.out.println("Cliente: " + c.getNombre() + " | Gastado: " + c.getDinero_gastado() + "€");
+            }
+            ClienteDAO dao5 = new ClienteDAO();
+            List<Cliente> listaOrdenada = dao.obtenerClientesOrdenadosPorGasto();
+
+            System.out.println("Clientes clasificados por gasto €");
+            for (Cliente cliente : listaOrdenada) {
+                System.out.println(c.getDinero_gastado() + "€ - " + c.getNombre());
+            }
+            ClienteDAO dao6 = new ClienteDAO();
+            List<Cliente> top3 = dao.obtenerTop3Compradores();
+
+            System.out.println("Podio de compradores");
+
+            int puesto = 1;
+            for (Cliente cliente : top3) {
+                System.out.println(puesto + "º Lugar: " + c.getNombre() + " (" + c.getProductos_comprados() + " productos)");
+                puesto++;
+            }
+        }
+        }
+
+
+
+
 
 
 
--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,9 +1,9 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.mysql.cj.xdevapi.PreparableStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
 
 
 public class ClienteDAO {
@@ -29,5 +29,214 @@ public class ClienteDAO {
         } catch (SQLException e) {
             System.out.println("Error al insertar cliente " + e.getMessage());
         }
+
     }
-}
+
+    public void actualizarCliente(Cliente cliente) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        String user = "root";
+        String password = "1234";
+
+        String sql = "UPDATE clientes SET id = ?, nombre = ?, email = ?, telefono = ?, edad = ?, dinero_gastado = ?, productos_comprados = ? WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(url, user, password);) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            {
+                pstmt.setInt(1, cliente.getId());
+                pstmt.setString(2, cliente.getNombre());
+                pstmt.setString(3, cliente.getEmail());
+                pstmt.setString(4, cliente.getTelefono());
+                pstmt.setInt(5, cliente.getEdad());
+                pstmt.setDouble(6, cliente.getDinero_gastado());
+                pstmt.setInt(7, cliente.getProductos_comprados());
+                // No se como hacer esta consulta.
+            }
+
+        }
+    }
+
+    public void eliminarCliente(Cliente cliente) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        String user = "root";
+        String password = "1234";
+
+        String sql = "DELETE FROM clientes WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(url, user, password);) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            {
+                pstmt.setInt(1, cliente.getId());
+
+                int filasAfectadas = pstmt.executeUpdate();
+
+                if (filasAfectadas > 0) {
+                    System.out.println("Cliente eliminado correctamente");
+                } else {
+                    System.out.println("Error al eliminar cliente " + cliente.getNombre());
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al eliminar cliente " + e.getMessage());
+        }
+    }
+
+    public Cliente obtenerCliente(int idBusqueda) throws SQLException {
+        Cliente cliente = null;
+
+
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        String user = "root";
+        String password = "1234";
+
+        String sql = "SELECT * FROM clientes WHERE id = ?";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, idBusqueda);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setEmail(rs.getString("email"));
+                cliente.setTelefono(rs.getString("telefono"));
+                cliente.setEdad(rs.getInt("edad"));
+                cliente.setDinero_gastado(rs.getDouble("dinero_gastado"));
+                cliente.setProductos_comprados(rs.getInt("productos_comprados")); {
+                }
+            }
+
+            } catch(SQLException e){
+            System.out.println("Error al obtener cliente " + e.getMessage());
+
+        }
+        return cliente;
+    }
+    public List<Cliente> obtenerTodosLosClientes() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        String user = "root";
+        String password = "1234";
+        List<Cliente> listaClientes = new ArrayList<>();
+
+        String sql = "SELECT * FROM clientes";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            Cliente cliente = new Cliente();
+
+            cliente.setId(rs.getInt("id"));
+            cliente.setNombre(rs.getString("nombre"));
+            cliente.setEmail(rs.getString("email"));
+            cliente.setTelefono(rs.getString("telefono"));
+            cliente.setEdad(rs.getInt("edad"));
+            cliente.setDinero_gastado(rs.getDouble("dinero_gastado"));
+            cliente.setProductos_comprados(rs.getInt("productos_comprados"));
+
+        listaClientes.add(cliente);
+        }
+            } catch (SQLException e) {
+            System.out.println("Error al listar " + e.getMessage());
+        }
+        return listaClientes;
+    }
+    public List<Cliente> obtenerMayoresDe30() {
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        String user = "root";
+        String password = "1234";
+        List<Cliente> listaFiltrada = new ArrayList<>();
+
+        String sql = "SELECT * FROM clientes WHERE edad > 30";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+                    Cliente cliente = new Cliente();
+                    cliente.setId(rs.getInt("id"));
+                    cliente.setNombre(rs.getString("nombre"));
+                    cliente.setEdad(rs.getInt("edad"));
+                    cliente.setEmail(rs.getString("email"));
+
+                    listaFiltrada.add(cliente);
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al filtrar " + e.getMessage());
+            }
+            return listaFiltrada;
+        }
+    public List<Cliente> obtenerClientesGastoMayor500() {
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        List<Cliente> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM clientes WHERE dinero_gastado > 500";
+
+        try (Connection conn = DriverManager.getConnection(url, "root", "1234");
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                c.setId(rs.getInt("id"));
+                c.setNombre(rs.getString("nombre"));
+                c.setDinero_gastado(rs.getDouble("dinero_gastado"));
+
+
+                lista.add(c);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al filtrar por gasto: " + e.getMessage());
+        }
+        return lista;
+    }
+    public List<Cliente> obtenerClientesOrdenadosPorGasto() {
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        List<Cliente> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM clientes ORDER BY dinero_gastado DESC";
+
+        try (Connection conn = DriverManager.getConnection(url, "root", "1234");
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                c.setId(rs.getInt("id"));
+                c.setNombre(rs.getString("nombre"));
+                c.setDinero_gastado(rs.getDouble("dinero_gastado"));
+
+                lista.add(c);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al ordenar clientes: " + e.getMessage());
+        }
+        return lista;
+    }
+    public List<Cliente> obtenerTop3Compradores() {
+        String url = "jdbc:mysql://localhost:3306/tienda";
+        List<Cliente> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM clientes ORDER BY productos_comprados DESC LIMIT 3";
+
+        try (Connection conn = DriverManager.getConnection(url, "root", "1234");
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                c.setId(rs.getInt("id"));
+                c.setNombre(rs.getString("nombre"));
+                c.setProductos_comprados(rs.getInt("productos_comprados"));
+
+                lista.add(c);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al obtener el Top 3: " + e.getMessage());
+        }
+        return lista;
+    }
+    }
+
